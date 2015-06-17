d130ed886160455832f1d22bdb9c2312c667fc01
Bob <lym@example.com>
2015-06-17 14:48:35 +0800

    merge to master

---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding=utf-8
+# 
+# Author:       liuyongming@nsfocus.com
+# 
+# Created Time: 2015年06月17日 星期三 14时29分08秒
+# 
+# FileName:     hello.py
+# 
+# Description:  
+# 
+# ChangeLog:
+
+print 'Hello world!'
